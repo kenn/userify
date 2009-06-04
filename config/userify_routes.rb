b62abf98d74b04003cf27f19102f5a916b78607a
Kenn Ejima <kenn@iMac24.local>
2009-06-04 01:51:22 -0700

    safer routes

--- config/userify_routes.rb
+++ config/userify_routes.rb
@@ -1,8 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.signup 'signup', :controller => 'userify/user', :action => 'signup'
-  map.signin 'signin', :controller => 'userify/user', :action => 'signin'
-  map.signout 'signout', :controller => 'userify/user', :action => 'signout'
-  map.activate 'activate/:token', :controller => 'userify/user', :action => 'activate'
-  map.forgot 'password/forgot', :controller => 'userify/user', :action => 'forgot'
-  map.reset 'password/reset/:token', :controller => 'userify/user', :action => 'reset'
+  map.signup 'user/signup', :controller => 'userify/user', :action => 'signup'
+  map.signin 'user/signin', :controller => 'userify/user', :action => 'signin'
+  map.signout 'user/signout', :controller => 'userify/user', :action => 'signout'
+  map.activate 'user/activate/:token', :controller => 'userify/user', :action => 'activate'
+  map.forgot 'user/password/forgot', :controller => 'userify/user', :action => 'forgot'
+  map.reset 'user/password/reset/:token', :controller => 'userify/user', :action => 'reset'
 end

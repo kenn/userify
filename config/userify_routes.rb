63372df2687f753c8c3f6405244848ae028e60b9
Kenn Ejima <kenn@iMac24.local>
2009-06-04 19:34:59 -0700

    okay, now routes are guessable enough

--- config/userify_routes.rb
+++ config/userify_routes.rb
@@ -3,6 +3,6 @@ ActionController::Routing::Routes.draw do |map|
   map.signin 'user/signin', :controller => 'userify/user', :action => 'signin'
   map.signout 'user/signout', :controller => 'userify/user', :action => 'signout'
   map.activate 'user/activate/:token', :controller => 'userify/user', :action => 'activate'
-  map.forgot 'user/password/forgot', :controller => 'userify/user', :action => 'forgot'
-  map.reset 'user/password/reset/:token', :controller => 'userify/user', :action => 'reset'
+  map.forgot 'user/forgot', :controller => 'userify/user', :action => 'forgot'
+  map.reset 'user/reset/:token', :controller => 'userify/user', :action => 'reset'
 end
